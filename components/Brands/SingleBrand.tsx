d86856ab1f8a263e081dfa86a0e6fdeb38b4bf1b
Tony <86345701+tonydisco@users.noreply.github.com>
2025-03-16 14:36:44 +0700

    brand

--- components/Brands/SingleBrand.tsx
+++ components/Brands/SingleBrand.tsx
@@ -1,46 +1,37 @@
-import React, { useRef, useEffect } from "react";
-import Image from "next/image";
 import { Brand } from "@/types/brand";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 const SingleBrand = ({ brand }: { brand: Brand }) => {
-  const { image, href, name, imageLight, id } = brand;
+  const { href, name, imageLight, id } = brand;
 
   return (
-    <>
-      <motion.a
-        variants={{
-          hidden: {
-            opacity: 0,
-            y: -20,
-          },
+    <motion.a
+      variants={{
+        hidden: {
+          opacity: 0,
+          y: -20,
+        },
 
-          visible: {
-            opacity: 1,
-            y: 0,
-          },
-        }}
-        initial="hidden"
-        whileInView="visible"
-        transition={{ duration: 1, delay: id }}
-        viewport={{ once: true }}
-        href={href}
-        className="animate_top mx-w-full relative block h-10 w-[98px]"
-      >
-        <Image
-          className="opacity-65 transition-all duration-300 hover:opacity-100 dark:hidden"
-          src={image}
-          alt={name}
-          fill
-        />
-        <Image
-          className="hidden opacity-50 transition-all duration-300 hover:opacity-100 dark:block"
-          src={imageLight}
-          alt={name}
-          fill
-        />
-      </motion.a>
-    </>
+        visible: {
+          opacity: 1,
+          y: 0,
+        },
+      }}
+      initial="hidden"
+      whileInView="visible"
+      transition={{ duration: 1, delay: id }}
+      viewport={{ once: true }}
+      href={href}
+      className="animate_top mx-w-full relative block h-10 w-[98px]"
+    >
+      <Image
+        className="hidden opacity-50 transition-all duration-300 hover:opacity-100 "
+        src={imageLight}
+        alt={name}
+        fill
+      />
+    </motion.a>
   );
 };
 
